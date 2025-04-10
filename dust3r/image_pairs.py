a06b1ae1dc3cf360cd2f731da5e37abf140ca9e8
junyi42 <junyi42zhang@gmail.com>
2025-04-10 21:11:13 +0000

    minor bug for fully feed-forward mode

--- dust3r/image_pairs.py
+++ dust3r/image_pairs.py
@@ -60,8 +60,7 @@ def make_pairs(imgs, scene_graph='complete', prefilter=None, symmetrize=True, fo
     elif scene_graph.startswith('oneref'):
         refid = int(scene_graph.split('-')[1]) if '-' in scene_graph else 0
         for j in range(len(imgs)):
-            if j != refid:
-                pairs.append((imgs[refid], imgs[j]))
+            pairs.append((imgs[refid], imgs[j]))
 
     if (symmetrize and not scene_graph.startswith('oneref') and not scene_graph.startswith('swin-1')) or len(imgs) == 2 or force_symmetrize:
         pairs += [(img2, img1) for img1, img2 in pairs]
